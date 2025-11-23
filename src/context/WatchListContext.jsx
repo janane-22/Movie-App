802fa45a6463bdd0c291479e69812f3e5c942a09
janane-22 <janujanane6@gmail.com>
2025-11-23 09:34:30 +0530

    fix: ESM tailwind config and silence react-refresh rule for context

--- src/context/WatchListContext.jsx
+++ src/context/WatchListContext.jsx
@@ -1,3 +1,5 @@
+
+/* eslint-disable react-refresh/only-export-components */
 import { Children, createContext, useState,useEffect } from "react";
 
 export const WatchListContext = createContext()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-    // tailwind.config.js
-    module.exports = {
-      content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
